bc1796d770a710d246aa35602791ada6d5534a21
zdd <602904354@q.com>
2019-08-05 00:59:00 +0800

    springboot2 dubbo upgrade

--- src/main/java/com/zdd/SpringbootDubboClientApplication.java
+++ src/main/java/com/zdd/SpringbootDubboClientApplication.java
@@ -1,9 +1,17 @@
 package com.zdd;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@Configuration
+@EnableDubbo(scanBasePackages = "com.zdd.dubbo.controller")
+@PropertySource("classpath:/spring/dubbo-consumer.properties")
+@ComponentScan(value = {"com.zdd.dubbo.controller"})
 public class SpringbootDubboClientApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/zdd/dubbo/controller/UserController.java
+++ src/main/java/com/zdd/dubbo/controller/UserController.java
@@ -1,17 +1,19 @@
-package com.zdd.controller;
+package com.zdd.dubbo.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.zdd.dubbo.UserService;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Component(value = "userController")
 @RestController
 public class UserController {
 
-    @Reference(version = "1.0.0")
+    @Reference
     UserService userService;
 
-    @GetMapping("check")
+    @GetMapping("/check")
     public String check(String name){
         return userService.check(name);
     }
